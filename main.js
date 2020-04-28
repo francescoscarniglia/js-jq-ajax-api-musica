b8245d19930bd986c8417b2e6766b9b817c8b753
francesco scarniglia <francescoscarniglia@gmail.com>
2020-04-28 18:18:20 +0200

    Ex.
    ok with Chiara.

--- main.js
+++ main.js
@@ -7,19 +7,28 @@
 //Endpoint API
 //https://flynn.boolean.careers/exercises/api/array/music
 $(document).ready(function() {
-
+var contentMex = $('.cds-container');
+var source = $('#cd-template').html();
 var apiBo = 'https://flynn.boolean.careers/exercises/api/array/music';
 
-var albums = [];
 
   $.ajax({
     url: apiBo,
     method: 'GET',
     success: function(data){
+
       var singles = data.response;
       console.log(singles);
-      if(data.success){
-        albums.push(singles)
+      if(singles.length > 0){
+        for(var i= 0; i < singles.length; i++) {
+          console.log(singles[i]);
+
+          var template = Handlebars.compile(source);
+
+          var html = template(singles[i]);
+
+          contentMex.append(html);
+        }
       }
     },
     error: function(){
@@ -28,8 +37,6 @@ var albums = [];
 
   });
 
-  var template = $('#cd-template').html();
-  var compiledTemplate = Handlebars.compile(template);
 
-  console.log(albums);
-});
+
+});//ready
